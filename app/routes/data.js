acb21849fccfe097e17862758b74a3af308d4b98
root <root@ip-10-60-22-133.eu-central-1.compute.internal>
2023-10-27 08:02:56 +0000

    updated Dynamo db client

--- app/routes/data.js
+++ app/routes/data.js
@@ -1,29 +1,40 @@
 const express = require('express');
 const AWS = require('aws-sdk');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 // Initialize DynamoDB client
 AWS.config.update({
     region: "eu-central-1"  // Adjust based on your region
  });
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+//const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 router.post('/data', async (req, res) => {
-    const { body } = req;
-    
-    const params = {
-        TableName: 'demo',  // replace with your DynamoDB table name
-        Item: body
+    const data = req.body;
+
+    // Generate a unique ID for the item
+    const id = uuidv4();
+
+    const item = {
+        id: id,
+        ...data
     };
 
+    // Use the item for DynamoDB put operation
     try {
-        await dynamoDB.put(params).promise();
-        res.status(201).json({ message: 'Data stored successfully.' });
+        await dynamoDb.put({
+            TableName: 'demo',
+            Item: item
+        }).promise();
+
+        res.json({ success: true, message: 'Data stored successfully', id: id });
     } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ message: 'Internal Server Error' });
+        res.status(500).json({ error: error.message });
     }
 });
 
 module.exports = router;
+
